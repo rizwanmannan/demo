b615cde4a80807812f41bd8bbfd93a08ed748abc
vikram <vikram#gmail.com>
2025-06-30 13:09:04 +0530

    This is for push

---
+++ src/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <>
+        <form action="" method=''>
+            <label for="name">Student Name</label>
+            <input type='text' placeholder='Student Name' name="name"/><br/>
+            <label for="phone">Mobile No</label>
+            <input type='text' placeholder='Enter Mobile No' name="phone"/><br/>
+            <button type='submit'>Save</button>
+
+        </form>
+    </>
+  )
+}
